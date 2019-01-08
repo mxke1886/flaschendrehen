32a9f13fdecc47eb6ee7d7ad0d7e0e81f219c135
mxke1886 <mike.meier12.mm@gmail.com>
2019-01-08 13:51:03 +0100

    Logik Fertig

--- app/src/main/java/ch/tbz/dablondes/flaschendrehen/MainActivity.java
+++ app/src/main/java/ch/tbz/dablondes/flaschendrehen/MainActivity.java
@@ -2,12 +2,56 @@ package ch.tbz.dablondes.flaschendrehen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView bottle;
+    private Random random = new Random();
+    private int lastDir;
+    private boolean spinning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottle = findViewById(R.id.bottle);
+    }
+
+    public void spinBottle(View v){
+        if (!spinning) {
+            int newDir = random.nextInt(1800);
+            float pivotX = v.getWidth() / 2;
+            float pivotY = v.getHeight() / 2;
+
+            Animation rotate = new RotateAnimation(lastDir, newDir, pivotX, pivotY);
+            rotate.setDuration(3000);
+            rotate.setFillAfter(true);
+
+            rotate.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spinning = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            lastDir = newDir;
+            bottle.startAnimation(rotate);
+        }
     }
 }
